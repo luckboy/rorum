d60e0a1ce46245be923f0cdc5659ffb193396e28
Lukasz Szpakowski <luckboy@vp.pl>
2009-03-07 15:03:54 +0100

    added setup with login_as and changed other methods

--- test/functional/roles_controller_test.rb
+++ test/functional/roles_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class RolesControllerTest < ActionController::TestCase
+  include AuthenticatedTestHelper
+
+  def setup
+    login_as :root
+  end
+
   def test_should_get_index
     get :index
     assert_response :success
@@ -14,30 +20,32 @@ class RolesControllerTest < ActionController::TestCase
 
   def test_should_create_role
     assert_difference('Role.count') do
-      post :create, :role => { }
+      post :create, :role => { :name => "user2" }
     end
 
-    assert_redirected_to role_path(assigns(:role))
+    # assert_redirected_to role_path(assigns(:role))
+    assert_redirected_to roles_path
   end
 
   def test_should_show_role
-    get :show, :id => roles(:one).id
+    get :show, :id => roles(:user).id
     assert_response :success
   end
 
   def test_should_get_edit
-    get :edit, :id => roles(:one).id
+    get :edit, :id => roles(:user).id
     assert_response :success
   end
 
   def test_should_update_role
-    put :update, :id => roles(:one).id, :role => { }
-    assert_redirected_to role_path(assigns(:role))
+    put :update, :id => roles(:user).id, :role => { }
+    # assert_redirected_to role_path(assigns(:role))
+    assert_redirected_to roles_path
   end
 
   def test_should_destroy_role
     assert_difference('Role.count', -1) do
-      delete :destroy, :id => roles(:one).id
+      delete :destroy, :id => roles(:user).id
     end
 
     assert_redirected_to roles_path
