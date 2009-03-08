33c036c3feb40e93b3598f6ad6281ccbe406d4b1
Lukasz Szpakowski <luckboy@vp.pl>
2009-03-08 13:08:17 +0100

    role_lists_controller_test and search_outputs_controller_test

--- test/functional/role_lists_controller_test.rb
+++ test/functional/role_lists_controller_test.rb
@@ -1,8 +1,31 @@
 require 'test_helper'
 
 class RoleListsControllerTest < ActionController::TestCase
+  include AuthenticatedTestHelper
+  fixtures :users
+
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_should_get_edit
+    login_as :root
+    get :edit, :id => users(:aaron).id
+    assert_response :success
+  end
+
+  def test_should_permission_denied_to_get_edit
+    get :edit, :id => users(:aaron).id
+    assert_redirected_to categories_path
+    assert_not_nil flash[:error]
+  end
+
+  def test_should_update_role_list
+    login_as :root
+    put :update,  :id => users(:aaron).id, :user => { }
+    assert_redirected_to profiles_path
+  end
+
+  def test_should_permission_denied_to_update_role_list
+    put :update,  :id => users(:aaron).id, :user => { }
+    assert_redirected_to categories_path
+    assert_not_nil flash[:error]
   end
 end
--- test/functional/search_outputs_controller_test.rb
+++ test/functional/search_outputs_controller_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class SearchOutputsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_should_get_new
+    get :new
+    assert_response :success
+  end
+
+  def test_should_create_search_output
+    post :create, :keywords => "MyText1", :author => "aaron", :forum => "", :search_in => "1"
+    assert_redirected_to search_output_topics_path
   end
 end
